972d624c9bba657063008287debe56d9c3a222a5
Guru Karthikeyan <gurukartkn@gmail.com>
2022-04-23 20:44:51 +0530

    HeroBanner

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,5 +1,24 @@
+import Link from "next/link";
 const HeroBanner = () => {
-  return <div>HeroBanner</div>;
+  return (
+    <div className="hero-banner-container">
+      <div>
+        <p className="beats-solo">Small Text</p>
+        <h3>Mid Text</h3>
+        <img src="" alt="headphones" className="hero-banner-image" />
+
+        <div>
+          <Link href="/product/ID">
+            <button type="button">Button Text</button>
+          </Link>
+          <div className="desc">
+            <h5>Description</h5>
+            <p>Description</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HeroBanner;
